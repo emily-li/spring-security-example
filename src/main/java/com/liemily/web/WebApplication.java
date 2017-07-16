5883bc00758828f95e6fe77d4318c42a86a1db80
emily-li <emily.li@live.co.uk>
2017-07-16 22:23:12 +0100

    Changing @EnableAutoConfiguration to @SpringBootApplication for better control

--- src/main/java/com/liemily/web/WebApplication.java
+++ src/main/java/com/liemily/web/WebApplication.java
@@ -1,7 +1,7 @@
 package com.liemily.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Created by Emily Li on 16/07/2017.
  */
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class WebApplication {
 
     public static void main(String[] args) {
