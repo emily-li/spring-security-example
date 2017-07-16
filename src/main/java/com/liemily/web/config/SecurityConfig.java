3db96977ec3d3dbff3c4ab9e5c1bc0728925c813
emily-li <emily.li@live.co.uk>
2017-07-16 22:23:33 +0100

    Adding SecurityConfig to allow access to all webpages

---
+++ src/main/java/com/liemily/web/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.liemily.web.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Emily Li on 16/07/2017
+ */
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/*").permitAll();
+    }
+}
