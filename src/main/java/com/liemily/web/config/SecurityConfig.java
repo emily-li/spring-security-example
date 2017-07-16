97c8e94d2ac7f5eda0aae62645d5ac6788d0d376
emily-li <emily.li@live.co.uk>
2017-07-16 22:26:43 +0100

    Securing page 'secure'

--- src/main/java/com/liemily/web/config/SecurityConfig.java
+++ src/main/java/com/liemily/web/config/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
+                .antMatchers("/secure").hasRole("USER")
                 .antMatchers("/*").permitAll();
     }
 }
