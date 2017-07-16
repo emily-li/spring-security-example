5faa03f3d390ba28c39388fe5fd63e8503f4f1f9
emily-li <emily.li@live.co.uk>
2017-07-16 21:02:00 +0100

    Creating simple Spring Boot starter

---
+++ src/main/java/com/liemily/WebApplication.java
@@ -0,0 +1,24 @@
+package com.liemily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * WebApplication Spring Boot starter
+ * Created by Emily Li on 16/07/2017.
+ */
+@RestController
+@EnableAutoConfiguration
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
