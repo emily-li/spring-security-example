3ed7e0fc249b5df2605750f9ef050924706e5e63
emily-li <emily.li@live.co.uk>
2017-07-18 23:02:27 +0100

    Adding performance tests and refactoring

--- src/test/java/com/liemily/web/WebApplicationTest.java
+++ src/test/java/com/liemily/web/WebApplicationTest.java
@@ -18,6 +18,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -43,7 +52,7 @@ public class WebApplicationTest {
     private String url;
 
     private final int AVG_RUN_COUNT = 100;
-    private final long MAX_REQUEST_WAIT_MS = 10;
+    private final long MAX_REQUEST_WAIT_MS = 1000;
 
     @Before
     public void setup() {
@@ -66,29 +75,29 @@ public class WebApplicationTest {
 
     @Test
     public void testSuccessfulLoginAvgTime() throws Exception {
-        long avg = 0;
-        for (int i = 0; i < AVG_RUN_COUNT; i++) {
-            avg += timePostForLogin(user, password);
-        }
-        avg /= AVG_RUN_COUNT;
-        logger.info("Average run time of successful logins (with " + AVG_RUN_COUNT + " runs) is " + avg + "ms");
-
-        assertThat(avg).isLessThan(MAX_REQUEST_WAIT_MS);
+        long time = timeParallelPostForLogin(user, password, AVG_RUN_COUNT, 1);
+        assertThat(time).isLessThan(MAX_REQUEST_WAIT_MS);
     }
 
     @Test
     public void testUnsuccessfulLoginAvgTime() throws Exception {
-        long avg = 0;
-        for (int i = 0; i < AVG_RUN_COUNT; i++) {
-            avg += timePostForLogin(user, "");
-        }
-        avg /= AVG_RUN_COUNT;
-        logger.info("Average run time of unsuccessful logins (with " + AVG_RUN_COUNT + " runs) is " + avg + "ms");
-
-        assertThat(avg).isLessThan(MAX_REQUEST_WAIT_MS);
+        long time = timeParallelPostForLogin(user, "", AVG_RUN_COUNT, 1);
+        assertThat(time).isLessThan(MAX_REQUEST_WAIT_MS);
     }
 
-    protected ResponseEntity<String> postForLogin(String user, String password) {
+    @Test
+    public void testParallelSuccessfulLoginAvgTime() throws Exception {
+        long time = timeParallelPostForLogin(user, password, AVG_RUN_COUNT, AVG_RUN_COUNT);
+        assertThat(time).isLessThan(MAX_REQUEST_WAIT_MS);
+    }
+
+    @Test
+    public void testParallelUnsuccessfulLoginAvgTime() throws Exception {
+        long time = timeParallelPostForLogin(user, "", AVG_RUN_COUNT, AVG_RUN_COUNT);
+        assertThat(time).isLessThan(MAX_REQUEST_WAIT_MS);
+    }
+
+    private ResponseEntity<String> postForLogin(String user, String password) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
@@ -101,13 +110,45 @@ public class WebApplicationTest {
         return restTemplate.postForEntity(url + "/login", request, String.class);
     }
 
-    protected long timePostForLogin(String user, String password) {
+    private long timePostForLogin(String user, String password) {
         long startTime = System.currentTimeMillis();
         postForLogin(user, password);
         long stopTime = System.currentTimeMillis();
+        return stopTime - startTime;
+    }
 
-        long time = stopTime - startTime;
-        logger.info("Post took " + time + "milliseconds");
-        return time;
+    private long timeParallelPostForLogin(String user, String password, int numTasks, int parallelThreads) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(parallelThreads);
+        Collection<LoginTask> loginTasks = new ArrayList<>();
+        IntStream.range(0, numTasks).forEach(i -> loginTasks.add(new LoginTask(user, password)));
+
+        long invokeStart = System.currentTimeMillis();
+        List<Future<Long>> times = executorService.invokeAll(loginTasks);
+
+        long avg = 0;
+        for (Future<Long> time : times) {
+            avg += time.get();
+        }
+        long invokeStop = System.currentTimeMillis();
+        avg /= AVG_RUN_COUNT;
+
+        logger.info("Average run time of login tasks was " + avg + "ms");
+        logger.info("All " + numTasks + " login tasks ran in " + (invokeStop - invokeStart) + "ms");
+        return avg;
+    }
+
+    private class LoginTask implements Callable<Long> {
+        private String user;
+        private String password;
+
+        LoginTask(String user, String password) {
+            this.user = user;
+            this.password = password;
+        }
+
+        @Override
+        public Long call() throws Exception {
+            return timePostForLogin(user, password);
+        }
     }
 }
