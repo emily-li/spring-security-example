2a3ecd2492efcfaae35ff2139fad75266fc441d2
emily-li <emily.li@live.co.uk>
2017-07-18 22:14:51 +0100

    Adding performance tests to ensure login requests do not take longer than MAX_REQUEST_WAIT_MS

--- src/test/java/com/liemily/web/WebApplicationTest.java
+++ src/test/java/com/liemily/web/WebApplicationTest.java
@@ -1,5 +1,7 @@
 package com.liemily.web;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +26,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class WebApplicationTest {
+    private static final Logger logger = LogManager.getLogger(WebApplicationTest.class);
 
     @LocalServerPort
     private int port;
@@ -39,9 +42,13 @@ public class WebApplicationTest {
 
     private String url;
 
+    private final int AVG_RUN_COUNT = 100;
+    private final long MAX_REQUEST_WAIT_MS = 10;
+
     @Before
     public void setup() {
         url = "http://localhost:" + port;
+        restTemplate.getForObject(url, String.class); // First server request initialises server components which takes longer, so run this before performance checks
     }
 
 
@@ -57,6 +64,30 @@ public class WebApplicationTest {
         assertThat(response.getHeaders().get("Location").get(0)).isEqualTo(url + "/login?error");
     }
 
+    @Test
+    public void testSuccessfulLoginAvgTime() throws Exception {
+        long avg = 0;
+        for (int i = 0; i < AVG_RUN_COUNT; i++) {
+            avg += timePostForLogin(user, password);
+        }
+        avg /= AVG_RUN_COUNT;
+        logger.info("Average run time of successful logins (with " + AVG_RUN_COUNT + " runs) is " + avg + "ms");
+
+        assertThat(avg).isLessThan(MAX_REQUEST_WAIT_MS);
+    }
+
+    @Test
+    public void testUnsuccessfulLoginAvgTime() throws Exception {
+        long avg = 0;
+        for (int i = 0; i < AVG_RUN_COUNT; i++) {
+            avg += timePostForLogin(user, "");
+        }
+        avg /= AVG_RUN_COUNT;
+        logger.info("Average run time of unsuccessful logins (with " + AVG_RUN_COUNT + " runs) is " + avg + "ms");
+
+        assertThat(avg).isLessThan(MAX_REQUEST_WAIT_MS);
+    }
+
     protected ResponseEntity<String> postForLogin(String user, String password) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -68,5 +99,15 @@ public class WebApplicationTest {
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
 
         return restTemplate.postForEntity(url + "/login", request, String.class);
+    }
+
+    protected long timePostForLogin(String user, String password) {
+        long startTime = System.currentTimeMillis();
+        postForLogin(user, password);
+        long stopTime = System.currentTimeMillis();
+
+        long time = stopTime - startTime;
+        logger.info("Post took " + time + "milliseconds");
+        return time;
     }
 }
